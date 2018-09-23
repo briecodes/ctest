b32815955f6a91704a2349dc84bc36c61b34bcf6
briecodes <hello@brie.work>
2018-09-23 17:13:38 -0400

    canvas image created. Tap to open in new browser defined.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let canvas = '';
+
 class App extends Component {
 
   componentDidMount() {
@@ -8,15 +10,32 @@ class App extends Component {
   };
 
   launchProgram = () => {
-    let canvas = document.getElementById('myCanvas');
-    let context = canvas.getContext('2d');
+    document.addEventListener('click', this.takeScreenshot );
+    canvas = document.getElementById('myCanvas');
+    // const text = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    context.fillStyle = 'blue';
-    context.font = '40pt Calibri';
-    context.fillText('Hello World!', 150, 100);
+    this.makeLine(0, 0, window.innerWidth/2, window.innerHeight/2);
+
+    // text.fillStyle = 'blue';
+    // text.font = '40pt Calibri';
+    // text.fillText('Hello World!', ((window.innerWidth/2) - 100), window.innerHeight/2);
   };
+
+  takeScreenshot = () => {
+    const dataURL = canvas.toDataURL();
+    console.log(dataURL);
+    window.open(dataURL);
+  };
+
+  makeLine = (a1, a2, b1, b2) => {
+    const line = canvas.getContext('2d');
+    line.beginPath();
+    line.moveTo(a1, a2);
+    line.lineTo(b1, b2);
+    line.stroke();
+  }
 
   render() {
     return (
