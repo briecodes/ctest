4dbf70b6da306d34fbf6cd717ce6c637c8794d4f
briecodes <hello@brie.work>
2018-11-04 17:52:03 -0500

    Rotation by device functional.

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-// let angleText = '';
-
 class Home extends Component {
 
   state = {
@@ -12,7 +10,6 @@ class Home extends Component {
 
   componentDidMount() {
     console.log('Home page loaded.');
-    // angleText = document.getElementById('angle');
 
     if (window.DeviceMotionEvent) {
       window.addEventListener('deviceorientation', e => this.deviceMotionHandler(e));
@@ -27,7 +24,7 @@ class Home extends Component {
 
   getSectionWidth = () => {
     const angleNum = 90;
-    const sections = angleNum/this.getImgCount();
+    const sections = angleNum/(((this.getImgCount()-2)/2)+2);
 
     return sections;
   };
@@ -36,83 +33,53 @@ class Home extends Component {
     return this.state.images.length;
   };
 
-  classHandler = (s, e) => {
-    if (s === '-'){
-      document.getElementById(e).classList.remove('hide-img');
+  classHandler = (sign, id) => {
+    if (sign === '-'){
+      document.getElementById(id).classList.remove('hide-img');
     } else {
-      document.getElementById(e).classList.add('hide-img');
+      document.getElementById(id).classList.add('hide-img');
     };
   };
 
-  cycleImages = (n) => {
-    for (const i of this.state.images) {
-      let index = this.state.images.indexOf(i);
-      if ( index === n ) {
-        this.classHandler('-', this.state.images[n]);
-        this.display('notes', `Image: ${this.state.images[n]}`);
+  cycleImages = (id) => {
+    for (const image of this.state.images) {
+      if ( image === id ) {
+        this.classHandler('-', image);
       } else {
-        this.classHandler('+', this.state.images[index]);
-        this.display('notes', `Image: ${this.state.images[index]}`);
+        this.classHandler('+', image);
       };
     };
   };
 
-  display = (e, c) => {
-    document.getElementById(e).innerHTML = c;
+  display = (id, content) => {
+    document.getElementById(id).innerHTML = content;
   };
 
   deviceMotionHandler = (evt) => {
-    // const num = Math.floor(evt.gamma);
     const num = evt.gamma;
-    this.display('angle', `Angle: ${num}. Sections: ${this.getSectionWidth()}`);
+    // this.display('angle', `Angle: ${Math.floor(num)}. Sections: ${this.getSectionWidth()}`);
 
-    this.circleDivider(num, this.getSectionWidth(), this.state.images, this.cycleImages)
-
-    // if (num >= -4 && num <= 4 ) {
-    //   // this.setState({ angle: 0 });
-    //   this.cycleImages(0);
-    // } else if (num < 4 && num >= -8 ) {
-    //   // this.setState({ angle: -1 });
-    //   this.cycleImages(5);
-    // } else if (num < -8 && num >= -16 ) {
-    //   // this.setState({ angle: -2 });
-    //   this.cycleImages(6);
-    // } else if (num < -16 && num >= -20 ) {
-    //   // this.setState({ angle: -3 });
-    //   this.cycleImages(7);
-    // } else if (num < -20 || num > 20 ) {
-    //   // this.setState({ angle: -4 });
-    //   this.cycleImages(4);
-    // } else if (num > 4 && num <= 8 ) {
-    //   // this.setState({ angle: 1 });
-    //   this.cycleImages(1);
-    // } else if (num > 8 && num <= 16 ) {
-    //   // this.setState({ angle: 2 });
-    //   this.cycleImages(2);
-    // } else if (num > 16 && num <= 20 ) {
-    //   // this.setState({ angle: 3 });
-    //   this.cycleImages(3);
-    // }
+    this.rotateImages(num, this.getSectionWidth(), this.state.images, this.cycleImages);
   };
 
-  circleDivider = (currentAngle, sectionWidth, imgArr, func) => {
-    this.display('addnotes', `inside circleDivider`);
+  rotateImages = (currentAngle, sectionWidth, imgArr, func) => {
     let i = 0;
-    for (const n of imgArr) {
-      let index = imgArr.indexOf(n) + 1;
+    const sideNum = ((imgArr.length-2)/2)+2;
 
-      if ( currentAngle > i * index && currentAngle < index * sectionWidth) {
-        // this.display('addnotes', `circleDivider. i: ${i}. index of i: ${this.state.images.indexOf(n)}. sectionWidth: ${sectionWidth}`);
-        func( this.state.images.indexOf(n) );
+    for (const n of imgArr) {
+      if ( currentAngle > i * sectionWidth && currentAngle < (i + 1) * sectionWidth) {
+        func( n );
         return;
-      } else if ( currentAngle < -1 *(i * index) && currentAngle > -1 * (index * sectionWidth)) {
-        this.display('addnotes', `circleDivider. i: ${i}. index of i: ${this.state.images.indexOf(n)}. first: ${-1 *(i * index)}. second: ${-1 * (index * sectionWidth)}`);
-        func( this.state.images.indexOf(n) );
+      } else if ( currentAngle < i * sectionWidth && currentAngle > -(i + 1) * sectionWidth) {
+        if (i < sideNum - 2) {
+          func( imgArr[sideNum + i] );
+        } else {
+          func( imgArr[sideNum - 1] );
+        }
         return;
       } else {
         i++
       };
-
     };
   };
 
@@ -132,18 +99,10 @@ class Home extends Component {
         </NavLink>
         <div id='pic'>
           {this.images('auto', '150px')}
-          {/* {this.state.angle === 0 ? <img src='./assets/0.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === -1 ? <img src='./assets/b1.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === -2 ? <img src='./assets/b2.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === -3 ? <img src='./assets/b-3.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === -4 ? <img src='./assets/ab.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === 1 ? <img src='./assets/a-1.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === 2 ? <img src='./assets/a-2.jpg' alt='pic' height='100' width='auto' /> : null }
-          {this.state.angle === 3 ? <img src='./assets/a-3.jpg' alt='pic' height='100' width='auto' /> : null } */}
         </div>
         <div id='angle'></div>
         <div id='notes'></div>
-        <div id='addnotes'></div>
+        <div id='addnote'></div>
       </center>
     );
   };
